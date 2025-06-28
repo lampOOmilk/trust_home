58d0a93c94d29ac5c444d00888d2a03ab4bb4ed3
lampOOmilk <lesha.ms2009@gmail.com>
2025-06-28 21:32:50 +0300

    Update chat.py

--- tg-bot/database_handler/chat.py
+++ tg-bot/database_handler/chat.py
@@ -9,12 +9,7 @@ async def open_db_request():
 
 async def upload_db_request(surname: str, name: str, tg_id: int, trast_id: int, text: str):
     # тут должно быть занесение запроса в базу
-    await bot.send_message(text=f'Поступила заявка на консультацию\n\n'
-                              f'От {surname} {name}\n'
-                              f'telegram id = {tg_id}, trast id = {trast_id}\n\n'
-                              f'Tекст запроса:\n'
-                              f'{text}',
-                           chat_id=921394541)
+    pass
 
 
 
